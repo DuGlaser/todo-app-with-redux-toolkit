7d3a67bbcc32cf631f2c51f2eee8fa12b7ded018
DuGlaser <duglaser0618@gmail.com>
2020-09-30 23:41:05 +0900

    :sparkles: Add the process to update todo item

--- src/components/organisms/todoList/index.tsx
+++ src/components/organisms/todoList/index.tsx
@@ -15,16 +15,33 @@ const card = css({
   borderRadius: '8px',
 });
 
+const doneCard = css(card, {
+  border: 'red 1px solid',
+});
+
 interface Props {
   todos: Todo[];
+  handleTodoDone: (todo: Todo) => void;
 }
 
 const TodoList: React.FC<Props> = (props) => {
-  const { todos } = props;
+  const { todos, handleTodoDone } = props;
   return (
     <div css={wrapper}>
       {todos.map((todo) => {
-        return <div css={card}>{todo.description}</div>;
+        return (
+          <React.Fragment key={todo.id}>
+            {todo.isDone ? (
+              <div css={doneCard} onDoubleClick={() => handleTodoDone(todo)}>
+                {todo.description}
+              </div>
+            ) : (
+              <div css={card} onDoubleClick={() => handleTodoDone(todo)}>
+                {todo.description}
+              </div>
+            )}
+          </React.Fragment>
+        );
       })}
     </div>
   );
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import * as UUID from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAllTodos } from '../../features/todo/todo.select';
-import { todoAddOne } from '../../features/todo/todo.slice';
+import { todoAddOne, todoUpdateOne } from '../../features/todo/todo.slice';
 import Header from '../organisms/header';
 import FormWrapper from '../organisms/formWrapper';
 import TodoList from '../organisms/todoList';
+import { Todo } from '../../features/todo/todo.entity';
 
 const Home: React.FC = () => {
   const [value, setValue] = useState('');
@@ -14,6 +15,12 @@ const Home: React.FC = () => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
+  };
+
+  const handleTodoDone = (todo: Todo) => {
+    dispath(
+      todoUpdateOne({ id: todo.id, changes: { ...todo, isDone: !todo.isDone } })
+    );
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -41,7 +48,7 @@ const Home: React.FC = () => {
         handleChange={handleChange}
         value={value}
       />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} handleTodoDone={handleTodoDone} />
     </>
   );
 };
